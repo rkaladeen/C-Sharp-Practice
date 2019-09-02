ea4a47b914710e4618092695d9e66f64ca6ba29b
raj-kaladeen <r.kaladeen@gmail.com>
2019-09-02 13:48:41 -0400

    added timeTable

---
+++ MultiDimArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiDimArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MDArray();
+        }
+        static void MDArray() 
+        {
+          int[,] timeTable = new int[11,11];
+          string tempStr = "";
+          
+          for(int row = 1; row <= 10; row++)
+          {
+            tempStr = "";
+            for(int col = 1; col <= 10; col++)
+            {
+              timeTable[row, col] = row*col;
+              tempStr += $"{timeTable[row, col]},";
+            }
+            Console.WriteLine($"[ {tempStr} ]");
+          }
+        }
+    }
+}
