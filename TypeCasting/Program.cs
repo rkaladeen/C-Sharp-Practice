9a6ab4f566fc70ecc1261488185707182bfa89c9
raj-kaladeen <r.kaladeen@gmail.com>
2019-09-02 21:32:05 -0400

    add type casting assignment

---
+++ TypeCasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TypeCasting
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Type Casting Example");
+      List<object> objs = new List<object>();
+      List<string> objsNewType = new List<string>();
+      objs.Add(7);
+      objs.Add(28);
+      objs.Add(-1);
+      objs.Add(true);
+      objs.Add("Chair");
+      int total = 0;
+      int temp = 0;
+      foreach(object obj in objs)
+      {
+        Console.WriteLine(obj);
+        if(obj is int)
+        {
+          temp = (int)obj;
+          total += temp;
+        }
+      }
+      Console.WriteLine("Total: "+total);
+    }
+  }
+}
