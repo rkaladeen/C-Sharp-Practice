7c1f337e50337eac15f7f44a2dad1d73f686d1f3
raj-kaladeen <r.kaladeen@gmail.com>
2019-09-04 20:44:24 -0400

    added class templates

--- HungryNinja/Program.cs
+++ HungryNinja/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace HungryNinja
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace HungryNinja {
+  class Program {
+    static void Main(string[] args) {
+      
     }
+  }
 }
